797101ddb31f3919c72274338b50b190a458b156
“Daniel Griffiths” <Danielgriffiths1790@gmail.com>
2024-05-27 18:56:02 +0100

    new nav

---
+++ assets/js/dropdown.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var dropdown = document.querySelector('.dropdown');
+    dropdown.addEventListener('mouseover', function() {
+        document.querySelector('.dropdown-content').style.display = 'block';
+    });
+    dropdown.addEventListener('mouseout', function() {
+        document.querySelector('.dropdown-content').style.display = 'none';
+    });
+});
---
+++ assets/js/loadNav.js
@@ -0,0 +1,20 @@
+window.onload = function() {
+    const navElement = document.getElementById('nav');
+    fetch('nav.html')
+        .then(response => response.text())
+        .then(html => {
+            navElement.innerHTML = html;
+            // Set active class after the nav is loaded
+            const navLinks = document.querySelectorAll('#nav .links li');
+            const currentPath = window.location.pathname.split('/').pop();
+
+            navLinks.forEach(link => {
+                if (link.querySelector('a').getAttribute('href').includes(currentPath)) {
+                    link.classList.add('active');
+                } else {
+                    link.classList.remove('active');
+                }
+            });
+        })
+        .catch(error => console.error('Error loading the navigation bar:', error));
+};
