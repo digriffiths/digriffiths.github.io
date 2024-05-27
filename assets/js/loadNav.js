32c5eb90bbceb88d9918122e0c276572fb1e3cbb
“Daniel Griffiths” <Danielgriffiths1790@gmail.com>
2024-05-27 19:04:56 +0100

    path updated

--- assets/js/loadNav.js
+++ assets/js/loadNav.js
@@ -1,10 +1,10 @@
 window.onload = function() {
     const navElement = document.getElementById('nav');
-    fetch('nav.html')
+    // Adjust the path as necessary, e.g., './nav.html' or '/repository/nav.html'
+    fetch('./nav.html')
         .then(response => response.text())
         .then(html => {
             navElement.innerHTML = html;
-            // Set active class after the nav is loaded
             const navLinks = document.querySelectorAll('#nav .links li');
             const currentPath = window.location.pathname.split('/').pop();
 
