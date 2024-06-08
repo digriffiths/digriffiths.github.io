9ae611741235c20741c6ce8fe3778f1ac050581c
“Daniel Griffiths” <Danielgriffiths1790@gmail.com>
2024-06-08 13:17:34 +0100

    media test

--- assets/js/loadNav.js
+++ assets/js/loadNav.js
@@ -1,19 +1,4 @@
-window.onload = function() {
-    const navElement = document.getElementById('nav');
-    fetch('./nav.html')
-        .then(response => response.text())
-        .then(html => {
-            navElement.innerHTML = html;
-            const navLinks = document.querySelectorAll('#nav ul li a'); // Corrected selector
-            const currentPath = window.location.pathname.split('/').pop();
-
-            navLinks.forEach(link => {
-                if (link.getAttribute('href').includes(currentPath)) {
-                    link.parentElement.classList.add('active'); // Add class to li, not a
-                } else {
-                    link.parentElement.classList.remove('active');
-                }
-            });
-        })
-        .catch(error => console.error('Error loading the navigation bar:', error));
-};
+function toggleMenu() {
+    var menu = document.querySelector('.nav-links');
+    menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
+}
